e6e1dae36de5eec15c83126cef2959c44d958d08
soldiersnake <mariano.maciasgandulfo@gmail.com>
2024-10-14 13:08:12 +0200

    modificacion de importacion, error en deploy

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,7 @@ import { useLoaderData } from "@remix-run/react";
 import TaskForm from "~/components/TaskForm";
 import { Header } from "~/components/Header";
 import { TaskType } from "~/types";
-import Tasks from "./Task";
+import Tasks from "./Tasks";
 
 export const meta: MetaFunction = () => {
   return [
